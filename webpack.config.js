c49e7cf19ad22c8e25b6141b142f9f9064b79585
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-05-02 14:08:44 +0600

    Add all files

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+  },
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+
+    ],
+
+  },
+};
